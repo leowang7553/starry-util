58dc2ba81e3fb28e90343d92cdb075b3e9993826
leowang7553 <leowang7553@163.com>
2018-05-03 10:36:10 +0800

    Add files via upload

---
+++ js/cookie.js
@@ -0,0 +1,47 @@
+// Cookie 对象
+let Cookie = {
+
+    /**
+    * cookie 设置方法
+    * @param {String} name 属性名
+    * @param {String} value 属性值
+    */
+    set: function(name, value) {
+        document.cookie = encodeURIComponent(name) + '='
+                          + encodeURIComponent(value);
+    },
+
+    /**
+    * cookie 获取方法
+    * @param {String} name 属性名
+    * @return {String} 返回对应的属性值
+    */
+    get: function(name) {
+        var nameStr = name + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(nameStr) == 0) {
+                return c.substring(nameStr.length, c.length);
+            }
+        }
+        return "";
+    },
+
+    /**
+    * cookie 删除方法
+    * @param {String} name 属性名
+    */
+    remove: function(name) {
+        document.cookie = encodeURIComponent(name) + '='
+        + '' + ';Expires=' + new Date(0);
+    }
+
+}
+
+
+module.exports = Cookie;
